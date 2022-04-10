ce8a4ca5d44af58880a4fba700f776cf38d267f7
kxyzkox <95051477+kxyzkox@users.noreply.github.com>
2022-04-10 16:15:23 +0200

    upload z git

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ window.onload = function() {
 
       var join_input = document.createElement('input')
       join_input.setAttribute('id', 'join_input')
-      join_input.setAttribute('maxlength', 15)
+      join_input.setAttribute('maxlength', 50)
       join_input.placeholder = 'Wpisz swoją nazwe'
       // Every time we type into the join_input
       join_input.onkeyup  = function(){
